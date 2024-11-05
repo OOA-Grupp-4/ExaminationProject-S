4f864b3a476a16fb95415d44deefeb61e3170e54
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-11-05 20:15:09 +0100

    Add project files.

---
+++ ObjectAnal/Models/BaseProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace Business.Models;
+
+public abstract class BaseProduct
+{
+    public string ProductId { get; set; } = Guid.NewGuid().ToString();
+    public string ProductName { get; set; } = null!;
+    public string ProductPrice { get; set; } = null!;
+
+}
+
+
+public class WishlistProduct : BaseProduct
+{
+    public bool Visible { get; set; } = true;
+}
---
+++ WishlistTesting/WishlistTesting.cs
@@ -0,0 +1,38 @@
+﻿namespace WishlistTesting;
+
+public class WishlistTesting
+{
+    /*
+    First test is to make sure that if the admin hides a product from the site, it should not show up in peoples wishlist.
+    -
+    Need a product with a "visible" property that is a bool that the admin will change.
+    Need a list of products. 
+    Need a list of things inside a wishlist.
+    -
+    A method that will check for visible products and return a list to the method that "shows the list to the user"..
+    -
+    A wishlistProduct that can be used.
+
+     */
+    [Fact]
+    public void AdminChangeVisibility_ShouldReturnSmallerList()
+    {
+        //Arrange
+
+
+
+        //Act
+
+
+
+        //Assert
+
+
+
+    }
+    [Fact]
+    public void AdminChangeVisibility_ShouldReturnALongerList()
+    {
+
+    }
+}
